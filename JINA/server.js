f4482a411bf21aad44200ffd9af8c1c7656fde26
xhbal <contactoxhbal@gmail.com>
2026-08-26 12:03:50 -0600

    Update server.js

--- JINA/server.js
+++ JINA/server.js
@@ -314,7 +314,7 @@ ${seccionNacionales}`;
         { role: 'user', content: promptOpcion4 }
       ],
       temperature: 0.1,
-      max_tokens: 8000
+      max_tokens: 20000
     }, { headers: { 'Authorization': `Bearer ${apiKey}`, 'Content-Type': 'application/json' }, timeout: 120000 });
 
     let textoRespuesta = response.data.choices[0].message.content;
